cf1cba178f64a4976ab3efa990693f4ed4c3ead2
Dawson Botsford <3408480+dawsbot@users.noreply.github.com>
2016-05-13 22:42:54 -0700

    update. keep minimal for now

--- app/templates/index.js
+++ app/templates/index.js
@@ -1,17 +1,10 @@
 'use strict';
-module.exports = function () {
-  const argLength = arguments.length;
-  const arg1 = arguments[0];
+module.exports = function (arg1) {
 
   // validate arguments
-  /*
   if (typeof arg1 !== 'string') {
     throw new TypeError(`Expected a string, got ${typeof arg1}`);
   }
-  */
-  if (argLength !== <%= argLength %>) {
-    throw new Error(`Expected <%= argLength %> arguments, got ${argLength}`);
-  }
 
-  return `${arg1} & ${argLength}`;
+  return true;
 };
